842585ff6ee645c04f73b7dea363eb37c7cbd65e
Olena Machushnyk <developer6.prog@gmail.com>
2020-10-12 00:33:22 +0300

    added project task7

---
+++ Task7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double sum = 0;
+            Console.Write("Введiть цiле число n (n>0): ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                sum += 1.0 / i;
+            }
+            Console.WriteLine($"Сума= {sum}");
+        }
+    }
+}
