077aa595b5eea95a58baf2389be04deba601da04
Olena Machushnyk <developer6.prog@gmail.com>
2020-10-11 23:40:15 +0300

    added project task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sideA, sideB, perimeter, square;
+            Console.Write("Введiть довжину сторони a прямокутника: ");
+            sideA = float.Parse(Console.ReadLine());
+            Console.Write("Введiть довжину сторони b прямокутника: ");
+            sideB = float.Parse(Console.ReadLine());
+            perimeter = 2 * (sideA + sideB);
+            Console.WriteLine($"Периметр= {perimeter}");
+            square = sideA * sideB;
+            Console.WriteLine($"Площа= {square}");
+            Console.ReadKey();
+        }
+    }
+}
