7691007119b6d14742de29e433cf9ff4f564cabf
Olena Machushnyk <developer6.prog@gmail.com>
2020-10-12 00:28:04 +0300

    added project task6

---
+++ Task6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double multipl = 1;
+            Console.Write("Введiть цiле число a (a<b): ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введiть цiле число b (a<b): ");
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++) {
+                multipl *= i;
+            }
+            Console.WriteLine($"Добуток= {multipl}");
+            Console.ReadKey();
+        }
+    }
+}
