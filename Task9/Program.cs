40dfe459ec395a54afa71ceaeb7685b14f7421d2
Olena Machushnyk <developer6.prog@gmail.com>
2020-10-12 01:03:49 +0300

    added project task9

---
+++ Task9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, copyI;
+            double sum = 0, faktorial = 1;
+            Console.Write("Введiть цiле число n (n>0): ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                faktorial = 1;
+                copyI = i;
+                while (copyI != 0) {
+                    faktorial *= copyI;
+                    copyI--;
+                }
+                sum += faktorial;
+            }
+            Console.WriteLine($"Сума= {sum}");
+            Console.ReadKey();
+        }
+    }
+}
