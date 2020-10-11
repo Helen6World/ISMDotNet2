633b048200115e11b4803fab48af5a165086d3f8
Olena Machushnyk <developer6.prog@gmail.com>
2020-10-12 00:55:50 +0300

    added project task8

---
+++ Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, result = 1;
+            int n;
+            Console.Write("Введiть дробове число a (a^n): ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Введiть цiле число n (a^n): ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                result *= a;
+            }
+            Console.WriteLine($"a^n= {result}");
+            Console.ReadKey();
+        }
+    }
+}
+
